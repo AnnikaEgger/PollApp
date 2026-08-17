3607ef5bba7c38fe96f642e12fec14c28e0290ee
Annika Egger <annikaegger83@gmail.com>
2026-08-17 11:57:03 +0200

    feat: adding correct routes for the landing page and each survey

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
-import { Survey } from './pages/survey-page/survey/survey';
+import { SurveyPage } from './pages/survey-page/survey-page';
+import { LandingPage } from './pages/landing-page/landing-page';
 
-export const routes: Routes = [{ path: 'surveyId', component: Survey }];
+export const routes: Routes = [
+  { path: '', component: LandingPage },
+
+  { path: 'survey/:id', component: SurveyPage },
+];
