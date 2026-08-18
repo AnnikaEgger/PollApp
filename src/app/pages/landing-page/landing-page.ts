57c3082c96107891206b7cbf8aab8aa565f6660b
Annika Egger <annikaegger83@gmail.com>
2026-08-18 12:28:51 +0200

    style: making the landing page responsive

--- src/app/pages/landing-page/landing-page.ts
+++ src/app/pages/landing-page/landing-page.ts
@@ -3,10 +3,11 @@ import { SURVEY_CATEGORIES } from '../../shared/constants/survey-categories';
 import { CreateSurveyPage } from '../create-survey-page/create-survey/create-survey';
 import { DropdownMenu } from '../../shared/components/dropdown-menu/dropdown-menu';
 import { SurveyCatalog } from '../../features/survey-catalog/survey-catalog';
+import { CreateSurveyBtn } from '../../shared/components/create-survey-btn/create-survey-btn';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [SurveyCatalog, DropdownMenu, CreateSurveyPage],
+  imports: [SurveyCatalog, DropdownMenu, CreateSurveyPage, CreateSurveyBtn],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.scss',
 })
